11d4e38054ae5c63af4a8a3c51bed7a308d431a8
varunms123 <varunmvsrin@gmail.com>
2026-05-19 17:29:55 +0530

    pushed all filed

--- App.tsx
+++ App.tsx
@@ -1,45 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import 'react-native-gesture-handler';
+import { SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import AppNavigator from "./src/navigations/AppNavigator";
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
+import { useEffect } from 'react';
+import { initializeAuth } from './src/store/slices/authSlice';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+function RootApContent(){
+  useEffect(() => {
+    store.dispatch(initializeAuth() as any);
+  }, []);
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+  return <AppNavigator/>
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
+export default function App(){
+  return(
+    <Provider store={store}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle='light-content' backgroundColor='#1E3A8A'/>
+          <RootApContent/>
+      </SafeAreaView>
+    </Provider>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-});
-
-export default App;
+    backgroundColor: '#1E3A8A'
+  }
+})
